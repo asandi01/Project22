54927e993efd0bbb19d30edfa606dff275800ce5
DESKTOP-CSE0K64\albin <asandi01@gmail.com>
2018-03-28 13:42:14 -0600

    Add project files.

---
+++ Project22/MainActivity.cs
@@ -0,0 +1,16 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace Project22 {
+    [Activity(Label = "Project22", MainLauncher = true)]
+    public class MainActivity : Activity {
+        protected override void OnCreate(Bundle savedInstanceState) {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+        }
+    }
+}
+
